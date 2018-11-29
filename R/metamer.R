aa99fb79e3ddbf1c6af91f7f35ce4bc4a7343de8
Unknown <eliocampitelli@gmail.com>
2018-11-29 00:35:49 -0300

    Just gooifin'

--- R/metamer.R
+++ R/metamer.R
@@ -1,22 +1,28 @@
-
-metamer <- function(data, FUN, cols, signif = 2,  N = 100, target = NULL, distance_fun,
-                    annealing = FALSE,...) {
+metamerise <- function(data, FUN, cols, signif = 2,  N = 100, target = NULL, distance_fun,
+                       annealing = FALSE,...) {
+  data <- as.data.frame(data)
   fun <- match.fun(FUN)
   org <- fun(data)
 
-  change <- dim(data[, c(cols)])
+  ncols <- length(cols)
+  nrows <- nrow(data)
+  npoints <- ncols*nrows
 
-  steps <- list()
+  # steps <- list(length = N)
 
-
+  steps <- vector(mode = "list", length = N)
+  on.exit(return(data.table::rbindlist(steps, idcol = TRUE)))
+  m <- 1
+  steps[[m]] <- data
   new_data <- data
   dist_new <- NULL
 
-  M_temp <- 0.4
+  M_temp <- 0.2
   m_temp <- 0.01
   i <- 1
+
   tries <- 1
-  max_tries <- 10000
+  max_tries <- 1000
   ann <- FALSE
 
   # Calculate distance
@@ -25,37 +31,37 @@ metamer <- function(data, FUN, cols, signif = 2,  N = 100, target = NULL, distan
   }
 
   while (i <= N && tries <= max_tries) {
-    temp <- M_temp + (i-1)/N*(m_temp - M_temp)
+
+    temp <- M_temp + (i-1)/(N-1)^2*(m_temp - M_temp)
     # Select point to change
-    point <- sample(1:nrow(new_data), nrow(new_data))
+    # point <- sample(1:nrow(new_data), nrow(new_data))
 
     # Perturb data
-    new_data[point, c(cols)] <- data[point, c(cols)] + rnorm(change[2], 0, 0.1/tries)
+    new_data[, c(cols)] <- data[, c(cols)] + matrix(rnorm(npoints, 0, 0.01),
+                                                    nrow = nrows, ncol = ncols)
     new <- fun(new_data)
 
-    # If statistics are close
-    if (all(round(new, signif) - round(org, signif) == 0)) {
-      # Compute new distance (if needed)
-      if (!is.null(target)) {
-        dist_new <- distance_fun(new_data[, c(cols)], target)
-      }
+    if (!is.null(target)) {
+      dist_new <- distance_fun(new_data[, c(cols)], target)
+    }
 
-      # If distance is smaller or is not needed
-      # if (isTRUE(annealing)) ann <- temp > runif(1)
-      if (is.null(target) || dist_new <= dist || ann ) {
+    if (isTRUE(annealing)) ann <- temp > runif(1)
+    if (is.null(target) || dist_new <= dist || ann ) {
+      if (all(round(new, signif) - round(org, signif) == 0)) {
+        # Compute new distance (if needed)
+
+        # If distance is smaller or is not needed
+
         # Save data and state
-        steps[[i]] <- new_data
+        m <- m + 1
+        steps[[m]] <- new_data
         data <- new_data
-
-        cat("Tried ", tries, " times and found the ", i, "th metamer with distance: ", dist_new, " (", dist_new/dist_org, ")\r",
+        cat("Found the ", m, "th metamer in the ", i, "th iteration with distance: ", dist_new, " (", dist_new/dist_org, ")\r",
             sep = "")
         dist <- dist_new
-        i <- i + 1
-        tries <- 1
       }
-    } else {
-      tries <- tries + 1
     }
+    i <- i + 1
   }
   data.table::rbindlist(steps, idcol = TRUE)
 }
