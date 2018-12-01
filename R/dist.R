65b11b4bfdbd6e1726d8ff7031600d89592e3da4
Unknown <eliocampitelli@gmail.com>
2018-12-01 00:29:49 -0300

    Works!

--- R/dist.R
+++ R/dist.R
@@ -1,5 +1,30 @@
 dist_2d_euclid <- function(data, target) {
   mean(vapply(seq_len(nrow(data)), function(i) {
-    min(with(data[i, ], (carat - target$carat)^2 + (price - target$price)^2))
+    sqrt(min(with(data[i, ], (x - target$x)^2 + (y - target$y)^2)))
   }, 1))
 }
+
+
+dist_euc <- function(data, target) {
+  mean(FNN::get.knnx(as.matrix(target), as.matrix(data), k = 1)$nn.dist)
+}
+
+
+# From https://stackoverflow.com/questions/35194048/using-r-how-to-calculate-the-distance-from-one-point-to-a-line
+.dist2d <- function(a, b, c) {
+  v1 <- b - c
+  v2 <- a - b
+  m <- cbind(v1,v2)
+  abs(det(m))/sqrt(sum(v1*v1))
+}
+
+dist2line <- function(data, lines) {
+  mean(vapply(1:nrow(lines), function(t) {
+    a <-  c(t(lines[t, 1:2]))
+    b <- c(t(lines[t, 3:4]))
+    min(vapply(1:nrow(data), function(i) {
+      .dist2d(c(t(data[i, 1:2])), a, b)
+    }, FUN.VALUE = 1))
+  }, FUN.VALUE = 1))
+}
+
--- R/metamer.R
+++ R/metamer.R
@@ -10,6 +10,8 @@ metamerise <- function(data, FUN, cols, signif = 2,  N = 100, target = NULL, dis
 
   # steps <- list(length = N)
 
+  R <- mean(c(diff(range(data$x)), diff(range(data$y))))
+
   steps <- vector(mode = "list", length = N)
   on.exit(return(data.table::rbindlist(steps, idcol = TRUE)))
   m <- 1
@@ -17,7 +19,7 @@ metamerise <- function(data, FUN, cols, signif = 2,  N = 100, target = NULL, dis
   new_data <- data
   dist_new <- NULL
 
-  M_temp <- 0.2
+  M_temp <- 0.4
   m_temp <- 0.01
   i <- 1
 
@@ -32,23 +34,23 @@ metamerise <- function(data, FUN, cols, signif = 2,  N = 100, target = NULL, dis
 
   while (i <= N && tries <= max_tries) {
 
-    temp <- M_temp + (i-1)/(N-1)^2*(m_temp - M_temp)
+    temp <- M_temp + ((i-1)/(N-1))^2*(m_temp - M_temp)
     # Select point to change
     # point <- sample(1:nrow(new_data), nrow(new_data))
 
     # Perturb data
-    new_data[, c(cols)] <- data[, c(cols)] + matrix(rnorm(npoints, 0, 0.01),
+    new_data[, c(cols)] <- data[, c(cols)] + matrix(rnorm(npoints, 0, 0.05),
                                                     nrow = nrows, ncol = ncols)
     new <- fun(new_data)
 
-    if (!is.null(target)) {
-      dist_new <- distance_fun(new_data[, c(cols)], target)
-    }
+    if (all(round(new, signif) - round(org, signif) == 0)) {
+      if (!is.null(target)) {
+        dist_new <- distance_fun(new_data[, c(cols)], target)
+      }
 
-    if (isTRUE(annealing)) ann <- temp > runif(1)
-    if (is.null(target) || dist_new <= dist || ann ) {
-      if (all(round(new, signif) - round(org, signif) == 0)) {
-        # Compute new distance (if needed)
+      if (isTRUE(annealing)) ann <- temp > runif(1)
+
+      if (is.null(target) || dist_new <= dist || ann ) {
 
         # If distance is smaller or is not needed
 
@@ -56,7 +58,7 @@ metamerise <- function(data, FUN, cols, signif = 2,  N = 100, target = NULL, dis
         m <- m + 1
         steps[[m]] <- new_data
         data <- new_data
-        cat("Found the ", m, "th metamer in the ", i, "th iteration with distance: ", dist_new, " (", dist_new/dist_org, ")\r",
+        cat("Found the ", m, "th Metamer in the ", i, "th iteration with distance: ", dist_new, " (", dist_new/dist_org, ")\r",
             sep = "")
         dist <- dist_new
       }
