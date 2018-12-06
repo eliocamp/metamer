f97e69dec123d9c594ebaafd6ddd7d75fc672d57
Elio Campitelli <elio.campitelli@cima.fcen.uba.ar>
2018-12-06 16:29:52 -0300

    First widget

---
+++ R/create_target.R
@@ -0,0 +1,84 @@
+library(shiny)
+library(miniUI)
+
+myGadgetFunc <- function(data = data.frame(x = runif(20), y = runif(20)),
+                         target = NULL) {
+
+  ui <- miniPage(
+    gadgetTitleBar("Draw your target figure"),
+    miniContentPanel(
+      # actionButton("reset", "reset"),
+      plotOutput("plot", width = "100%", height = "100%",
+                 hover = hoverOpts(id = "hover", delay = 100,
+                                   delayType = "throttle", clip = TRUE, nullOutside = TRUE),
+                 click = "click"),
+      miniButtonBlock(
+        actionButton("reset_prev", "Clear last group"),
+        actionButton("reset_all", "Clear all")
+      )
+      # Define layout, inputs, outputs
+    )
+  )
+
+  server <- function(input, output, session) {
+    # Define reactive expressions, outputs, etc.
+    if (is.null(target)) {
+      target = reactiveValues(x = NULL, y = NULL, group = NULL)
+    } else {
+      target = reactiveValues(x = target$x, y = target$y, group = 1)
+    }
+
+    drawing = reactiveVal(FALSE)
+    current_group <- reactiveVal(1)
+
+    observeEvent(input$click, handlerExpr = {
+      if (drawing()) {
+        next_group <- current_group() + 1
+        current_group(next_group)
+      }
+      temp <- drawing()
+      drawing(!temp)
+
+      })
+
+    observeEvent(input$reset_all, handlerExpr = {
+      target$x <- NULL
+      target$y <- NULL
+      current_group(1)
+    })
+    observeEvent(input$reset_prev, handlerExpr = {
+      target$x <- target$x[target$group == current_group() - 1]
+      target$y <- target$y[target$group == current_group() - 1]
+      temp <- current_group()
+      # print(temp)
+      current_group(temp - 1)
+    })
+
+    observeEvent(input$hover, {
+      if (drawing()) {
+        target$x <- c(target$x, input$hover$x)
+        target$y <- c(target$y, input$hover$y)
+        gr <- current_group()
+        # print(gr)
+        target$group <- c(target$group, gr)
+      }})
+
+    output$plot <- renderPlot({
+      plot(x = data$x, y = data$y, ylab="y", xlab="x")
+      points(x = target$x, y = target$y)
+    })
+
+    # When the Done button is clicked, return a value
+    observeEvent(input$done, {
+      returnValue <- data.frame(x = target$x,
+                                y = target$y,
+                                group = target$group)
+      stopApp(returnValue)
+    })
+    observeEvent(input$cancel, {
+      stopApp(invisible(NULL))
+    })
+  }
+
+  runGadget(ui, server, stopOnCancel = FALSE)
+}
